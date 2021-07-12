43fb2e43259f0a7a2b29314a05e0acc49e64a70b
Bhawani Shankar <henryroy40@gmail.com>
2021-07-12 16:02:18 +0530

    .

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
+const User = require("../models/User");
+
 class AuthController {
-  static login = (req, res) => {
-    console.log("main function called");
-    var firstnumber = parseFloat(req.body.first);
-    var secondnumber = parseFloat(req.body.second);
-    var total = firstnumber + secondnumber;
-    console.log(total);
-    return res.render("index", { data: [{ data: "name" }, 1, 2, 3, 4] });
-    // return res.send("ervpo9ir");
+  static login = async (req, res) => {
+    const user = User({
+      name: "Himashu",
+    });
+    await user.save();
+
+    return res.send("ervpo9ir");
   };
 }
 
--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const mongoose = require("mongoose");
 const router = require("express").Router();
 const { checkLoggedIn } = require("./middlewares/auth");
 const AuthController = require("./controllers/auth");
-const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT;
+const MONGOURL = process.env.MONGOURL;
 
 app.set("view engine", "ejs");
 
+//mongodb connection
+mongoose.connect(MONGOURL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
+
 router.post("/", checkLoggedIn, AuthController.login);
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(router);
 
