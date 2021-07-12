23be00f32aa9169fd0222ed84355c1db2644b655
Bhawani Shankar <henryroy40@gmail.com>
2021-07-12 16:03:44 +0530

    .

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,12 +2,8 @@ const User = require("../models/User");
 
 class AuthController {
   static login = async (req, res) => {
-    const user = User({
-      name: "Himashu",
-    });
-    await user.save();
-
-    return res.send("ervpo9ir");
+    const users = await User.find();
+    return res.send(users);
   };
 }
 
